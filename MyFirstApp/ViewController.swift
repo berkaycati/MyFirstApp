687f4ab577cbc1231decf3c29ddf4cf2664121e2
berkaycati <berkaycati@hotmail.com>
2022-11-02 20:34:09 +0300

    Initial commit

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tittleItself: UILabel!
+    @IBOutlet weak var imageItself: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func clickButton(_ sender: Any) {
+        
+        imageItself.image = UIImage(named: "g1")
+    }
 }
 
